e0c75ebc6f49702c8754a35dbed1bf050fbbcc1f
David Yim <yimd85@gmail.com>
2018-02-04 09:30:30 -0500

    updated functionality: panhandlers and animated views

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, PanResponder, Animated } from 'react-native';
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: () => {}
+    });
+    this.state = { panResponder, position };
+  }
+
+  getCardStyle() {
+    return {
+      ...this.state.position.getLayout(),
+      transform: [{ rotate: '45deg' }]
+    };
+  }
+
   renderCards() {
-    return this.props.data.map(item => { 
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+        <Animated.View
+          key={item.id}
+          style={this.getCardStyle()}
+          {...this.state.panResponder.panHandlers}
+        >
+          {this.props.renderCard(item)}
+        </Animated.View>
+        );
+      }
       return this.props.renderCard(item);
     });
   }
 
   render() {
     return (
+
       <View>
           {this.renderCards()}
       </View>
